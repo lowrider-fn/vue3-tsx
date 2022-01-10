f2bae234bc8a4aae83c05b67abdeb9aeb18fda04
lowrider-fn <sergei.dolmatskii@gmail.com>
2022-01-10 15:22:14 +0300

    docs

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,8 @@
-const path = require('path');
-
 module.exports = {
   devServer: { open: true },
+  publicPath: (process.env.NODE_ENV === 'production' && 'vue3-tsx') || '/',
 
-  publicPath: process.env.NODE_ENV === ('production' && 'vue3-tsx') || '/',
-
-  outputDir: path.resolve(__dirname, './docs'),
+  outputDir: 'docs',
 
   css: { loaderOptions: { scss: { prependData: '@import "~@/assets/styles/index.scss";' } } },
 };
